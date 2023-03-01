1bcd65dd0f5f08b40f219b1d05556130407294ea
shanvit7 <shanvit.shetty@iotric.com>
2023-03-02 01:13:10 +0530

    test cases added

---
+++ web/tests/landing.test.jsx
@@ -0,0 +1,10 @@
+import { describe,test,expect } from 'vitest';
+import {render, screen} from '@testing-library/react';
+import Landing from '../src/pages/Landing'
+
+describe('Landing Page test',()=>{
+    test('Page should be shown',()=>{
+        render(<Landing/>);
+        expect(screen.getByText(/Let's dribble/i)).toBeDefined();
+    })
+})
