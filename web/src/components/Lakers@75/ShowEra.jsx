7b1dc5ddec7e461a6e3eba69ac32986b6aa5dcbe
shanvit <shanvit7@gmail.com>
2023-05-24 16:25:11 +0530

    Celebrations page animations added

--- web/src/components/Lakers@75/ShowEra.jsx
+++ web/src/components/Lakers@75/ShowEra.jsx
@@ -1,40 +1,98 @@
 import React from "react";
 import { ParallaxLayer } from "@react-spring/parallax";
 import Footer from "../Common/Footer";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 const ShowEra = ({ timeEra }) => {
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+  });
+  const [imgRef, inViewImg] = useInView({
+    triggerOnce: true,
+  });
+  const [quoteRef, inViewQuote] = useInView({
+    triggerOnce: true,
+  });
   return (
     <>
       <ParallaxLayer offset={timeEra.id + 1} speed={1.5}>
         <div className={`h-full flex flex-col bg-black`}>
-          <div className="py-32">
-           <h1 className="text-center font-bold">{timeEra.title}</h1>
-           <h2 className="text-center mt-8 text-xl">{timeEra.period}</h2>
+          <div ref={ref} className="py-32">
+            {inView && (
+              <>
+                <motion.h1
+                  className="text-center font-bold text-sm lg:text-5xl"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1, scale: 1.4 }}
+                  transition={{ delay: 0.2, duration: 1.5 }}
+                >
+                  {timeEra.title}
+                </motion.h1>
+                <motion.h2
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ delay: 0.4, duration: 1.5 }}
+                  className="text-center mt-8 text-xs lg:text-xl"
+                >
+                  {timeEra.period}
+                </motion.h2>
+              </>
+            )}
           </div>
-          {
-            timeEra.points.map((point,index)=>(
-              <div key={index} className="p-2 flex mt-4 flex-col lg:flex-row">
-              <img
-                src={point.imgSrc}
-                className="max-w-sm rounded-lg shadow-2xl"
-                alt="logo"
-              />
-              <p className="flex flex-col p-2 justify-center items-center text-xs lg:text-2xl lg:p-8">
-                {point.description}
-              </p>
+          {timeEra.points.map((point, index) => (
+            <div
+              ref={imgRef}
+              key={index}
+              className="p-2 flex mt-4 flex-col lg:flex-row"
+            >
+              {inViewImg && (
+                <>
+                  <motion.img
+                    src={point.imgSrc}
+                    className="max-w-sm rounded-lg shadow-2xl"
+                    alt="logo"
+                    initial={{ opacity: 0, x: -300 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ delay: 0.5, duration: 1 }}
+                  />
+                  <motion.p
+                    className="flex flex-col p-2 justify-center items-center text-xs lg:text-2xl lg:p-8"
+                    initial={{ opacity: 0, x: 300 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ delay: 0.5, duration: 2 }}
+                  >
+                    {point.description}
+                  </motion.p>
+                </>
+              )}
             </div>
-            ))
-          }
-          {timeEra.id === 7
-           &&
-          <>
-          <blockquote className="border-l-4 flex flex-col justify-center items-center border-gray-400 pl-4 italic my-4 mt-32">
-              <p className="mb-2">
-              I'm here. I'm not going anywhere. No matter what the injury -- unless it's completely debilitating -- I'm going to be the same player I've always been. I'll figure it out. I'll make some tweaks, some changes, but I'm still coming.
-              </p>
-              <footer className="text-gray-500">- Kobe Bryant</footer>
-            </blockquote>
-           <Footer />
-          </>}
+          ))}
+          {timeEra.id === 7 && (
+            <>
+              <blockquote
+                ref={quoteRef}
+                className="border-l-4 flex flex-col justify-center items-center border-gray-400 pl-4 italic my-4 mt-32"
+              >
+                {inViewQuote && (
+                  <motion.section
+                  initial={{opacity: 0}}
+                  animate={{opacity: 1}}
+                  transition={{ delay: 0.2, duration: 2 }}
+                  >
+                    <p className="mb-2">
+                      I'm here. I'm not going anywhere. No matter what the
+                      injury -- unless it's completely debilitating -- I'm going
+                      to be the same player I've always been. I'll figure it
+                      out. I'll make some tweaks, some changes, but I'm still
+                      coming.
+                    </p>
+                    <footer className="text-gray-500">- Kobe Bryant</footer>
+                  </motion.section>
+                )}
+              </blockquote>
+              <Footer />
+            </>
+          )}
         </div>
       </ParallaxLayer>
     </>
--- web/src/pages/LakersCelebration.jsx
+++ web/src/pages/LakersCelebration.jsx
@@ -5,31 +5,41 @@ import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import ShowEra from "../components/Lakers@75/ShowEra";
 import { lakersTimeLine } from "../utils/lakersTimeline";
 import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 const LakersCelebration = () => {
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+  });
   return (
     <div className="overflow-hidden h-screen w-screen  bg-black">
       <Parallax pages={10}>
       <SideBarWrapper>
         <ParallaxLayer>
          <TopNavbar />
-         <div className="flex flex-col justify-center items-center h-full">
-         <motion.h1 
-         className="text-center font-bold"
-         initial={{ opacity: 0 }}
-         animate={{ opacity: 1 }}
-         transition={{delay:0.5,duration: 2}}
-         >
-         Witness the Rise
-          </motion.h1>
-          <motion.h2 
-          className="mt-8 text-center"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{delay:0.8,duration: 2}}
-         >
-            A Journey of Legends Unveiled
-          </motion.h2>
+         <div ref={ref} className="flex flex-col justify-center items-center h-full">
+         {inView && (
+            <motion.div
+              className="flex flex-col justify-center items-center h-full"
+            >
+              <motion.h1 
+               initial={{ opacity: 0 }}
+               animate={{ opacity: 1,scale: 1.2 }}
+               transition={{delay:0.5,duration: 2}}
+               className="text-center font-bold text-md lg:text-6xl"
+              >
+                Witness the Rise
+              </motion.h1>
+              <motion.h2 
+               initial={{ opacity: 0 }}
+               animate={{ opacity: 1 }}
+               transition={{delay:0.8,duration: 2}}
+               className="mt-8 text-center"
+              >
+                A Journey of Legends Unveiled
+              </motion.h2>
+            </motion.div>
+          )}
           </div>
         </ParallaxLayer>
         </SideBarWrapper>
