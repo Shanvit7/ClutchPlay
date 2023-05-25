9d755e13c03db9009905fe0335740998e9e0de83
shanvit <shanvit7@gmail.com>
2023-05-25 16:14:36 +0530

    back button showed in smaller devices

--- web/src/components/Common/BackButton.jsx
+++ web/src/components/Common/BackButton.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 const BackButton = () => {
   const navigate = useNavigate();
   return (
-    <div className="ml-4 mt-2 hidden xl:block">
+    <div className="ml-4 mt-2">
       <button className='bg-black hover:border-white' onClick={() => navigate(-1)}>
         <img src={"/left-arrow-back-svgrepo-com.svg"} className="w-8 h-8" alt='back-button'/>
       </button>
